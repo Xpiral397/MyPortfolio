2c6712c6ef94d02d9508c746b79308bc92df9e6d
Xpiral397 <111965825+Xpiral397@users.noreply.github.com>
2024-08-07 14:03:02 +0100

    my portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
   icons: {
-    icon: "/product/logo.png",
+    icon: "/logo.png",
   },
 };
 
